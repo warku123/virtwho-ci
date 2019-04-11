82fdf9c919a3d3d1bdc386837cc52931df72b692
Yuefen Liu <yuefliu@redhat.com>
2019-04-11 18:14:29 +0800

    add tier2 cases tc_2001,2002,2003,2004,2005

---
+++ tests/virtwho/tier2/tc_2005_validate_password_option_by_cli.py
@@ -0,0 +1,86 @@
+# coding:utf-8
+from virt_who import *
+from virt_who.base import Base
+from virt_who.register import Register
+from virt_who.testing import Testing
+
+class Testcase(Testing):
+    def test_run(self):
+        self.vw_case_info(os.path.basename(__file__), case_id='RHEL-136574')
+        hypervisor_type = self.get_config('hypervisor_type')
+        if hypervisor_type in ('libvirt-local', 'vdsm'):
+            self.vw_case_skip(hypervisor_type)
+        if self.pkg_check(self.ssh_host(), 'virt-who')[9:15] >= '0.23.3':
+            self.vw_case_skip("virt-who version")
+        self.vw_case_init()
+
+        # Case Config
+        results = dict()
+        if hypervisor_type == 'libvirt-remote':
+            hypervisor_type = 'libvirt'
+        base_cli = self.vw_cli_base() + '-d'
+
+        # Case Steps
+        logger.info(">>>step1: password option is good value")
+        data, tty_output, rhsm_output = self.vw_start(base_cli, exp_send=1)
+        res = self.op_normal_value(data, exp_error=0, exp_thread=1, exp_send=1)
+        results.setdefault('step1', []).append(res)
+
+        logger.info(">>>step2: password option is wrong value")
+        cli = self.vw_cli_base_update(base_cli, "--{0}-password=.*".format(hypervisor_type), "--{0}-password=xxxxx".format(hypervisor_type))
+        data, tty_output, rhsm_output = self.vw_start(cli)
+        if "libvirt" in hypervisor_type:
+            logger.warning("libvirt-remote can use sshkey to connect, password value is not necessary")
+            res1 = self.op_normal_value(data, exp_error=0, exp_thread=1, exp_send=1)
+            results.setdefault('step2', []).append(res1)
+        else:
+            msg_list = ["Unable to login|incorrect user.*|Authentication failure|Incorrect.*username|Unauthorized|Error.* backend|Permission denied"]
+            res1 = self.op_normal_value(data, exp_error="1|2|3", exp_thread=1, exp_send=0)
+            res2 = self.msg_validation(rhsm_output, msg_list, exp_exist=True)
+            results.setdefault('step2', []).append(res1)
+            results.setdefault('step2', []).append(res2)
+
+        logger.info(">>>step3: password option is 红帽€467aa value")
+        cli = self.vw_cli_base_update(base_cli, "--{0}-password=.*".format(hypervisor_type), "--{0}-password=红帽€467aa".format(hypervisor_type))
+        data, tty_output, rhsm_output = self.vw_start(cli)
+        if "libvirt" in hypervisor_type:
+            logger.warning("libvirt-remote can use sshkey to connect, password value is not necessary")
+            res1 = self.op_normal_value(data, exp_error=0, exp_thread=1, exp_send=1)
+            results.setdefault('step2', []).append(res1)
+        else:
+            msg_list = ["Unable to login|incorrect user.*|Authentication failure|Incorrect.*username|Unauthorized|Error.* backend|Permission denied"]
+            res1 = self.op_normal_value(data, exp_error="1|2|3", exp_thread=1, exp_send=0)
+            res2 = self.msg_validation(rhsm_output, msg_list, exp_exist=True)
+            results.setdefault('step3', []).append(res1)
+            results.setdefault('step3', []).append(res2)
+
+        logger.info(">>>step4: password option is null value")
+        cli = self.vw_cli_base_update(base_cli, "--{0}-password=.*".format(hypervisor_type), "--{0}-password= ".format(hypervisor_type))
+        data, tty_output, rhsm_output = self.vw_start(cli)
+        if "libvirt" in hypervisor_type:
+            logger.warning("libvirt-remote can use sshkey to connect, username is not necessary")
+            res1 = self.op_normal_value(data, exp_error=0, exp_thread=1, exp_send=1)
+            results.setdefault('step4', []).append(res1)
+        else:
+            msg_list = ["PASSWORD.* not set|virt-who can't be started"]
+            res1 = self.op_normal_value(data, exp_error="0|1|2", exp_thread=0, exp_send=0)
+            res2 = self.msg_validation(tty_output, msg_list, exp_exist=True)
+            results.setdefault('step4', []).append(res1)
+            results.setdefault('step4', []).append(res2)
+
+        logger.info(">>>step5: password option is disable")
+        cli = self.vw_cli_base_update(base_cli, "--{0}-password=.*".format(hypervisor_type), " ")
+        data, tty_output, rhsm_output = self.vw_start(cli)
+        if "libvirt" in hypervisor_type:
+            logger.warning("libvirt-remote can use sshkey to connect, username is not necessary")
+            res1 = self.op_normal_value(data, exp_error=0, exp_thread=1, exp_send=1)
+            results.setdefault('step5', []).append(res1)
+        else:
+            msg_list = ["PASSWORD.* not set|virt-who can't be started"]
+            res1 = self.op_normal_value(data, exp_error="0|1|2", exp_thread=0, exp_send=0)
+            res2 = self.msg_validation(tty_output, msg_list, exp_exist=True)
+            results.setdefault('step5', []).append(res1)
+            results.setdefault('step5', []).append(res2)
+
+        # Case Result
+        self.vw_case_result(results)
