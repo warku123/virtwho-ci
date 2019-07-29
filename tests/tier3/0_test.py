e68fefd3de03cb908090308eaf3cd2e10065d71b
root <root@yuefliu.nay.redhat.com>
2019-07-29 17:09:50 +0800

    delete useless tests/tier3/0_test.py

--- tests/tier3/0_test.py
+++
@@ -1,8 +0,0 @@
-type = 'https_proxy'
-hypervisor_type = 'libvirt'
-if (type == 'http_proxy' and 'hyperv' not in hypervisor_type) \
-        or (type == 'https_proxy' and hypervisor_type in ('kubevirt', 'libvirt', 'hyperv')):
-# if type == 'http_proxy':
-    print('true')
-else:
-    print('false')
