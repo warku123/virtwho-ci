8ce65752e922f439a7fda3ef4d00675ad71b739f
Hong Sun <hsun@redhat.com>
2020-11-17 11:27:31 +0800

    add new case for tc_1112 to check http_proxy (#108)

--- tests/tier1/tc_1112_check_http_proxy_option_by_etc_conf.py
+++ tests/tier1/tc_1112_check_http_proxy_option_by_etc_conf.py
@@ -6,22 +6,28 @@ from virt_who.testing import Testing
 
 class Testcase(Testing):
     def test_run(self):
-        self.vw_case_info(os.path.basename(__file__), case_id='RHEL-133704')
+        self.vw_case_info(os.path.basename(__file__), case_id='RHEL-196077')
         self.vw_case_init()
 
         # case config
         results = dict()
-        config_file = "/etc/sysconfig/virt-who"
-        self.vw_option_enable('[global]', '/etc/virt-who.conf')
-        self.vw_option_enable('debug', '/etc/virt-who.conf')
-        self.vw_option_update_value('debug', 'True', '/etc/virt-who.conf')
+        config_file = "/etc/virt-who.conf"
+        self.vw_option_enable('[global]', config_file)
+        self.vw_option_enable('debug', config_file)
+        self.vw_option_update_value('debug', 'True', config_file)
         self.vw_etc_d_mode_create('virtwho-config', '/etc/virt-who.d/virtwho-config.conf')
         squid_server = "squid.corp.redhat.com:3128"
         bad_squid_server = "squid.corp.redhat.com:9999"
-        steps = {'step1':'http_proxy', 'step2':'https_proxy'}
+        steps = {'step2':'http_proxy', 'step3':'https_proxy'}
         error_msg = ["Connection refused|Cannot connect to proxy|Connection timed out"]
 
         # case steps
+        logger.info(">>>step1: enable [system_environment] in /etc/virt-who.conf")
+        self.vw_option_enable("[system_environment]", config_file)
+        data, tty_output, rhsm_output = self.vw_start(exp_send=1)
+        res1 = self.op_normal_value(data, exp_error=0, exp_thread=1, exp_send=1)
+        results.setdefault('step1', []).append(res1)
+
         for step, option in sorted(steps.items(),key=lambda item:item[0]):
             logger.info(">>>{0}: run virt-who to check {1}".format(step, option))
             if option == "http_proxy":
@@ -31,7 +37,8 @@ class Testcase(Testing):
                 value = "https://{0}".format(squid_server)
                 bad_value = "https://{0}".format(bad_squid_server)
             logger.info("> run virt-who with good {0}".format(option))
-            self.vw_option_add(option, value, config_file)
+            self.vw_option_enable(option, config_file)
+            self.vw_option_update_value(option, value, config_file)
             data, tty_output, rhsm_output = self.vw_start(exp_send=1)
             s1 = self.op_normal_value(data, exp_error=0, exp_thread=1, exp_send=1)
             s2 = self.vw_msg_search(rhsm_output, "Connection built.*{0}".format(squid_server), exp_exist=True)
@@ -48,20 +55,14 @@ class Testcase(Testing):
             results.setdefault(step, []).append(s4)
             results.setdefault(step, []).append(s5)
 
-            logger.info(">>> run virt-who with no_proxy and bad proxy")
-            self.vw_option_add("no_proxy", "*", config_file)
+            logger.info(">>> run virt-who with bad proxy, but no_proxy=*")
+            self.vw_option_enable('no_proxy', config_file)
+            self.vw_option_update_value('no_proxy', '*', config_file)
             data, tty_output, rhsm_output = self.vw_start(exp_send=1)
             s6 = self.op_normal_value(data, exp_error=0, exp_thread=1, exp_send=1)
-            self.vw_option_del("no_proxy", config_file)
             results.setdefault(step, []).append(s6)
-
-            logger.info("> run virt-who with NO_PROXY and bad proxy")
-            self.vw_option_add("NO_PROXY", "*", config_file)
-            data, tty_output, rhsm_output = self.vw_start(exp_send=1)
-            s6 = self.op_normal_value(data, exp_error=0, exp_thread=1, exp_send=1)
-            self.vw_option_del(option, config_file)
-            self.vw_option_del("NO_PROXY", config_file)
-            results.setdefault(step, []).append(s6)
+            self.vw_option_disable(option, config_file)
+            self.vw_option_disable("no_proxy", config_file)
 
         # case result
         self.vw_case_result(results)
