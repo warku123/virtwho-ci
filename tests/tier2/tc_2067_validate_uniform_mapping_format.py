ebc71f8250d7a9dc7c094e3b667dacdaf5c0ccea
YuefenLiu <yuefliu@redhat.com>
2021-10-20 15:26:41 +0800

    update tc_2067 to support vCenter7.0

--- tests/tier2/tc_2067_validate_uniform_mapping_format.py
+++ tests/tier2/tc_2067_validate_uniform_mapping_format.py
@@ -25,7 +25,6 @@ class Testcase(Testing):
         # case steps
         logger.info(">>>step1: create json file")
         self.vw_fake_json_create("virt-who -d -p", json_file)
-        ret, fake_json = self.runcmd("cat {0}".format(json_file), self.ssh_host())
         self.vw_etc_d_delete_all()
 
         logger.info(">>>step2: check the mapping in json file is same with debug output")
@@ -34,7 +33,10 @@ class Testcase(Testing):
         else:
             self.vw_fake_conf_create(fake_config_file, json_file, is_hypervisor=True)
         data, tty_output, rhsm_output = self.vw_start(cli='virt-who -do', exp_send=1)
-        results.setdefault('step2', []).append(fake_json in tty_output)
+        ret, fake_json = self.runcmd("cat {0}".format(json_file), self.ssh_host())
+        fake_json_lines = fake_json.split('\n')
+        for line in fake_json_lines:
+            results.setdefault('step2', []).append(line in tty_output)
 
         # case result
         self.vw_case_result(results)
